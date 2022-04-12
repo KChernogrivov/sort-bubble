571bd7ea89d6874d302f58f9fff26fa4590c5d4c
Kirill <kirill.2201@inbox.ru>
2022-04-12 22:48:01 +0700

    default bubble

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let mass = [];
 for(let i = 0; i < 100; i++) mass[i] = Math.floor(Math.random() * 100);
 
-for(let i = 0; i < mass.length; i++) {
-    for(let n = i+1; n < mass.length; n++) {
-        if(mass[i] < mass[n]) {
-            let temp = mass[n]
-            mass[n] = mass[i]
-            mass[i] = temp
+or(let i = 0; i < mass.length; i++) {
+    for(let j = 0; j < mass.length - 1; j++) {
+        if(mass[j] > mass[j+1]) {
+            let temp = mass[j]
+            mass[j] = mass[j + 1]
+            mass[j + 1] = temp
         }
     }
 }
